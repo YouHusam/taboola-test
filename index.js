c760c5bc9979f77b57543b31a685f4e7519198af
Husam Younis <youhusam@gmail.com>
2024-04-03 23:29:09 +0300

    add editor config

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 (function () {
-    const SCRIPTS_TO_APPEND = [
-        { src: "src/html-wrapper.js" }
-    ];
+  const SCRIPTS_TO_APPEND = [
+    { src: "src/html-wrapper.js" }
+  ];
 
-    document._taboola = {};
+  document._taboola = {};
 
-    document.addEventListener("DOMContentLoaded", function () {
-        SCRIPTS_TO_APPEND.forEach(function (script) {
-            var scriptElement = document.createElement("script");
-            scriptElement.setAttribute("src", "//localhost:8080/" + script.src);
-            document.body.appendChild(scriptElement);
-        });
+  document.addEventListener("DOMContentLoaded", function () {
+    SCRIPTS_TO_APPEND.forEach(function (script) {
+      var scriptElement = document.createElement("script");
+      scriptElement.setAttribute("src", "//localhost:8080/" + script.src);
+      document.body.appendChild(scriptElement);
     });
+  });
 })();
