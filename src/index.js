0b9d2adbd8e693633ea11379312124e8e621ce3c
Husam Younis <youhusam@gmail.com>
2024-04-05 15:34:23 +0300

    Add api

--- src/index.js
+++ src/index.js
@@ -1,14 +1,53 @@
 (function () {
+  const TABOOLA_API = "https://api.taboola.com/1.0/json";
+
   const SCRIPTS_TO_APPEND = [
     { src: "src/widget-wrapper.js" }
   ];
   const STYLESHEETS_TO_APPEND = [
     { src: "src/widget.css" }
-  ];
+  ]
 
-  document._taboola = {};
+  /**
+   * Create Taboola API URL generator
+   * @param publisherId
+   * @param appType
+   * @param apiKey
+   * @returns {function({sourceType: string, sourceId: string, sourceUrl: string, count: number}): string}
+   */
+  const createApiUrl = function (publisherId, appType, apiKey) {
+    /**
+     * Get Taboola API URL
+     * @param urlOptions {Object} - URL options
+     * @param urlOptions.sourceType {String} - Source type
+     * @param urlOptions.sourceId {String} - Source ID
+     * @param urlOptions.sourceUrl {String} - Source URL
+     * @param urlOptions.count {Number} - Number of recommendations to fetch
+     */
+    return function (urlOptions) {
+      const sourceType = urlOptions.sourceType || "video";
+      const sourceId = urlOptions.sourceId || "";
+      const sourceUrl = urlOptions.sourceUrl || "";
+      const count = urlOptions.count || 4;
+      return TABOOLA_API + "/" + publisherId + "/recommendations.get" +
+        "?app.type=" + appType + "&app.apikey=" + apiKey +
+        "&source.type=" + sourceType + "&source.id=" + sourceId + "&source.url=" + sourceUrl
+        + "&count=" + count;
+    }
+  };
 
-  document.addEventListener("DOMContentLoaded", function () {
+  /**
+   * Initialize Taboola API
+   * @param options {Object} - Configuration options
+   * @param options.publisherId {String} - Taboola publisher ID
+   * @param options.appType {String} - Taboola app type
+   * @param options.apiKey {String} - Taboola API key
+   */
+  function init(options) {
+    if (!(options.publisherId && options.appType && options.apiKey)) {
+      throw new Error("Taboola API requires publisherId, appType and apiKey");
+    }
+
     SCRIPTS_TO_APPEND.forEach(function (script) {
       const scriptElement = document.createElement("script");
       scriptElement.setAttribute("src", "//localhost:8080/" + script.src);
@@ -20,5 +59,18 @@
       linkElement.setAttribute("href", "//localhost:8080/" + stylesheet.src);
       document.head.appendChild(linkElement);
     });
-  });
+    window.taboola.getApiUrl = createApiUrl(options.publisherId, options.appType, options.apiKey);
+  }
+
+  /**
+   * Expose the api to the global scope
+   * @type {{init: init, getApiUrl: function({sourceType: string, sourceId: string, sourceUrl: string}): string}}
+   * @public
+   */
+  window.taboola = {
+    init: init,
+    getApiUrl: function () {
+      throw new Error("Taboola API not initialized");
+    }
+  };
 })();
--- src/widget-wrapper.js
+++ src/widget-wrapper.js
@@ -1,32 +1,32 @@
 (function () {
   const WIDGET_CONTAINER_CLASS = 'taboola-widget-container';
 
-  const widgetHtml = `
-    <h5 class="widget-title">
-      Ad Content <span class="widget-title__badge">By Taboola</span>
-    </h5>
-    <div class="widget-content">
-    </div>
-`;
+  const widgetHtml =
+    "<h5 class=\"widget-title\">" +
+      "Ad Content<span class=\"widget-title__badge\">By Taboola</span>" +
+    "</h5>" +
+    "<div class=\"widget-content\"></div>"
+  ;
 
   // Add widgets to the DOM
   function addWidgets() {
     const widgetContainers = document.getElementsByClassName(WIDGET_CONTAINER_CLASS);
-    Array.from(widgetContainers).forEach((container) => {
-      if (container.className.includes(`${ WIDGET_CONTAINER_CLASS }--loaded`)) {
+      const loadedClassName = WIDGET_CONTAINER_CLASS + "--loaded";
+    Array.prototype.forEach.call(widgetContainers, function (container) {
+      if (container.className.includes(loadedClassName)) {
         return;
       }
       container.innerHTML = widgetHtml.trim();
-      container.className += ` ${ WIDGET_CONTAINER_CLASS }--loaded`;
+      container.className += " " + loadedClassName;
     });
   }
 
   // Listen to dynamically added DOM content eg React components...
   // Create a new observer instance
-  const observer = new MutationObserver((mutations) => {
-    mutations.forEach(() => {
+  const observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function () {
       console.log('DOM content updated');
-      document._taboola.addWidgets();
+      addWidgets();
     });
   });
 
@@ -36,6 +36,5 @@
   observer.observe(document.body, config);
 
   // Expose the function to the global scope
-  document._taboola.addWidgets = addWidgets;
   addWidgets();
 })();
